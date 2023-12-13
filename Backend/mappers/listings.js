61ff43b472484f8bb16f723593abf553907bd825
Matias <matiasbenincasa@xpertai.com>
2023-12-13 12:43:49 -0300

    Add existing project files to Git

---
+++ Backend/mappers/listings.js
@@ -0,0 +1,17 @@
+const config = require('config');
+
+const mapper = (listing) => {
+  console.log(listing);
+  const baseUrl = config.get('assetsBaseUrl');
+  const mapImage = (image) => ({
+    url: `${baseUrl}${image.fileName}_full.jpg`,
+    thumbnailUrl: `${baseUrl}${image.fileName}_thumb.jpg`,
+  });
+
+  return {
+    ...listing,
+    images: listing.images.map(mapImage),
+  };
+};
+
+module.exports = mapper;
